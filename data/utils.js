aa7692c8301e8a93abb69d6670fdc3ef2114e742
MrFranksJr <frankyjr@me.com>
2023-03-31 00:13:14 +0200

    winning function

--- data/utils.js
+++ data/utils.js
@@ -1,7 +1,41 @@
-export { preLoadImage }
+export { preLoadImage, reloadDefaultCards, calcWinner }
 
 function preLoadImage(imagePath, cardOnTable) {
     const drawnCard = new Image()
     drawnCard.src = imagePath
     document.getElementById(cardOnTable).src = drawnCard.src
+}
+
+function reloadDefaultCards() {
+    document.getElementById('card-1').src = "/img/card-back.png"
+    document.getElementById('card-2').src = "/img/card-back.png"
+}
+
+function calcWinner(drawnCards) {
+    const cardValues = {
+        'A': 14,
+        'K': 13,
+        'Q': 12,
+        'J': 11,
+        '0': 10,
+        '9': 9,
+        '8': 8,
+        '7': 7,
+        '6': 6,
+        '5': 5,
+        '4': 4,
+        '3': 3,
+        '2': 2
+      }
+
+      const card1Score = cardValues[drawnCards[0].code.slice(0, -1)]
+      const card2Score = cardValues[drawnCards[1].code.slice(0, -1)]
+
+      if (card1Score > card2Score) {
+        return 'Computer wins!! Score: ' + card1Score
+      } else if (card2Score > card1Score) {
+        return 'You win!! Score: ' + card2Score
+      } else {
+        return 'Tie!! Score: ' + card1Score
+      }
 }
--- index.js
+++ index.js
@@ -1,46 +1,43 @@
-import { preLoadImage } from "./data/utils.js"
+import { preLoadImage, reloadDefaultCards, calcWinner } from "./data/utils.js"
 
 let deckId
 const baseUrl = 'https://deckofcardsapi.com/api/deck'
 const deckCount = 'deck_count=1'
+const remainingCardsEl = document.getElementById('remaining-cards')
 
 function getDeck() {
     fetch(baseUrl+"/new/shuffle/?"+deckCount)
         .then(res => res.json())
         .then(data => {
             console.log(data)
-            document.getElementById('remaining-cards').innerHTML = `${data.remaining} Cards Remaining in Deck`
+            remainingCardsEl.innerHTML = `${data.remaining} Cards Remaining in Deck`
+            reloadDefaultCards()
             deckId = data.deck_id
         })
 }
 
 function drawCards() {
-    if (deckId) {
-        fetch(`${baseUrl}/${deckId}/draw/?count=2`)
-            .then(res => res.json())
-            .then(data => {
-                console.log(data)
-                document.getElementById('remaining-cards').innerHTML = `${data.remaining} Cards Remaining in Deck`
-                let i = 0
-                for (let card of data.cards) {
-
-                    if (i === 0) {
-                        i++
-                        document.getElementById('card-1').src = "/img/card-back.png"
-                        preLoadImage(card.image, 'card-1') 
-                    } 
-                    else if (i === 1) {
-                        document.getElementById('card-2').src = "/img/card-back.png"
-                        preLoadImage(card.image, 'card-2')
-                    }
+    fetch(`${baseUrl}/${deckId}/draw/?count=2`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            console.log(calcWinner(data.cards))
+            remainingCardsEl.innerHTML = `${data.remaining} Cards Remaining in Deck`
+            let i = 0
+            reloadDefaultCards()
+            for (let card of data.cards) {
+                if (i === 0) {
+                    i++
+                    preLoadImage(card.image, 'card-1') 
+                } 
+                else if (i === 1) {
+                    preLoadImage(card.image, 'card-2')
                 }
-        })
-    } else {
-        alert('Shuffle a new deck first!')
-    }
+            }
+    })
 }
 
 document.getElementById("new-deck").addEventListener("click", getDeck)
 document.getElementById("new-cards").addEventListener("click", drawCards)
 
-getDeck()
+window.onload = getDeck()
